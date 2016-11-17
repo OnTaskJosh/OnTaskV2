830a3e9e4f5145b44bfb2e910306ad381b1ee23f
Joshua Carroll <Joshua Carroll@JC-WINDOWS-MACB>
2016-11-17 13:23:50 -0600

    added "GetDriverVolumeByHour" function to HistoricDataService

--- OnTaskV2/BLL/HistoricDataService.cs
+++ OnTaskV2/BLL/HistoricDataService.cs
@@ -27,15 +27,47 @@ namespace OnTaskV2.BLL
         public decimal[] GetDriverVolumeBy15min(string driver, int storeNum, DateTime date)
         {
             var volumes = new decimal[96];
-            var volumeData = db.HistoricData.Where(d => d.StoreNumber == storeNum && d.DriverName == driver && d.Date == date.Date);
+            var volumeData = db.HistoricData.Where(d => d.StoreNumber == storeNum && d.DriverName == driver && DbFunctions.TruncateTime(d.Date) == DbFunctions.TruncateTime(date.Date));
             int minuteInc = 0, hourInc = 0;
+            List<HistoricData> incVolData = new List<HistoricData>();
             for (int i = 0; i < 96; i++) //loop through 15 minute increments.
             {
                 minuteInc = (i % 4)*15;
                 hourInc = i / 4;
                 try
                 {
-                    volumes[i] = volumeData.Where(d => DbFunctions.CreateTime(d.Time.Value.Hour, d.Time.Value.Minute, d.Time.Value.Second) == DbFunctions.CreateTime(hourInc, minuteInc, 0)).First().Quantity; //get specific 15-minute increment
+                    volumes[i] = 0;
+                    incVolData = volumeData.Where(d => DbFunctions.CreateTime(d.Time.Value.Hour, d.Time.Value.Minute, 0) == DbFunctions.CreateTime(hourInc, minuteInc, 0)).ToList(); //get specific 15-minute increment
+                    foreach(var dataItem in incVolData)
+                    {
+                        volumes[i] += dataItem.Quantity;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("{0} Exception caught.", e);
+                     //if no data, set to 0
+                }
+            }
+            return volumes;
+        }
+
+        public decimal[] GetDriverVolumeByHour(string driver, int storeNum, DateTime date)
+        {
+            var volumes = new decimal[24];
+            var volumeData = db.HistoricData.Where(d => d.StoreNumber == storeNum && d.DriverName == driver && DbFunctions.TruncateTime(d.Date) == DbFunctions.TruncateTime(date.Date));
+            int hourInc = 0;
+            List<HistoricData> hourVolData = new List<HistoricData>();
+            for (int i = 0; i < 24; i++) //loop through 15 minute increments.
+            {
+                try
+                {
+                    volumes[i] = 0;
+                    hourVolData = volumeData.Where(d => DbFunctions.CreateTime(d.Time.Value.Hour, 0, 0) == DbFunctions.CreateTime(hourInc, 0, 0)).ToList(); //get all hour increment
+                    foreach(var dataItem in hourVolData)
+                    {
+                        volumes[i] += dataItem.Quantity;
+                    }
                 }
                 catch (Exception e)
                 {
@@ -85,6 +117,7 @@ namespace OnTaskV2.BLL
     public interface IHistoricDataService
     {
         decimal[] GetDriverVolumeBy15min(string driver, int storeNum, DateTime date);
+        decimal[] GetDriverVolumeByHour(string driver, int storeNum, DateTime date);
         decimal GetDriverVolumeByDay(string driver, int storeNum, DateTime date);
         decimal GetDriverVolumeByWeek(string driver, int storeNum, DateTime date);
     }
